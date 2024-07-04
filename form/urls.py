390831945ce8680ea1b9aebd974f4de37ce29672
Keerthana-0524 <146005248+Keerthana-0524@users.noreply.github.com>
2024-07-04 10:58:51 +0530

    Add files via upload

---
+++ form/urls.py
@@ -0,0 +1,8 @@
+# form/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user_profile/', include('text.urls')),
+]
